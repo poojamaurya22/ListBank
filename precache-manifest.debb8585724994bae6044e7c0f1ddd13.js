24a78fd7d1e8c93869f453d64ea9815190f022e0
Pooja Maurya <poojamaurya22@gmail.com>
2020-05-18 16:45:30 +0530

    Updates

---
+++ precache-manifest.debb8585724994bae6044e7c0f1ddd13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517c0023e5ab3900233759a7d30e7f9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b3511a3d152ceffffd",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "000cc039276863e6e112",
+    "url": "/static/css/main.e77fa169.chunk.css"
+  },
+  {
+    "revision": "37b3511a3d152ceffffd",
+    "url": "/static/js/2.2408040b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.2408040b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "000cc039276863e6e112",
+    "url": "/static/js/main.df9dbf24.chunk.js"
+  },
+  {
+    "revision": "445f28833966bd908f2b",
+    "url": "/static/js/runtime-main.88f58571.js"
+  }
+]);
